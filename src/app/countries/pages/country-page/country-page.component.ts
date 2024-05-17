681cfabb99a1e35caf7177c27bb89046f388c2d0
José Manuel Martínez Antonio <josema.k303@gmail.com>
2024-05-17 14:44:57 -0600

    Countries Fin Seccion 9

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Countries } from '../../interfaces/countries';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css'
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
 
+  public country? : Countries;
+  constructor(private activatedRoute: ActivatedRoute,
+    private countriesService:CountriesService,
+    private router: Router
+  ){
+
+  }
+  ngOnInit(): void {
+      this.activatedRoute.params
+      .pipe(
+        switchMap(({id})=> this.countriesService.searchByAlphaCode(id))
+      )
+      .subscribe(
+        (countryArray)=>
+           {
+              if(!countryArray){
+                return this.router.navigateByUrl('by-country')
+              }
+              return this.country= countryArray;
+          })
+  }
+  searchCountry( code: string){
+    this.countriesService.searchByAlphaCode(code)
+        .subscribe(country => console.log(country))
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Countries } from '../interfaces/countries';
 
 @Injectable({
@@ -10,6 +10,17 @@ export class CountriesService {
 
   private apiUrl: string= 'https://restcountries.com/v3.1/';
   constructor(private httClient: HttpClient) { }
+
+
+  searchByAlphaCode( query:string): Observable<Countries | null>{
+    const url=`${this.apiUrl}alpha/${query}`;
+    //hacemos la peticion a traves de un observable el cual no se disparara hata que nos suscribimos en el componente que consume el servicio
+    return this.httClient.get<Countries[]>( url )
+    .pipe(
+      map(countries => countries.length > 0 ? countries[0]: null),
+      catchError((e) => of(null))
+    );
+  }
 
   searchCapital( query:string): Observable<Countries[]>{
     const url=`${this.apiUrl}capital/${query}`;
